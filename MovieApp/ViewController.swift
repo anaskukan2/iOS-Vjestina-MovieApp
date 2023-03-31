9eaaa9f1b5c17388ae312e8d30cf2eb01f21b8e5
anaskukan2 <ana.skukan7@gmail.com>
2023-03-31 19:05:49 +0200

    first commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by endava-bootcamp on 31.03.2023..
+//
+import UIKit
+import MovieAppData
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let details = MovieUseCase().getDetails(id: 111161)
+        print(details)
+        
+    }
+}
+
