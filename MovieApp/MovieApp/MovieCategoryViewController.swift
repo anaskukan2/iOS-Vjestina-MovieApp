5c8512bba9c9f2f46eb1f4d38c4f504a114a2cac
Ana Skukan <128219989+anaskukan2@users.noreply.github.com>
2023-04-12 00:41:05 +0200

    Add files via upload
    -ne radi drugi ekran

---
+++ MovieApp/MovieApp/MovieCategoryViewController.swift
@@ -0,0 +1,79 @@
+import Foundation
+import PureLayout
+import UIKit
+import MovieAppData
+import SDWebImage
+
+class MovieCategoriesViewController: UIViewController {
+    
+    private var collectionView: UICollectionView!
+    private var categories: [[MovieModel]]!
+    private var categorieTitles: [String]!
+    let details = MovieUseCase()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buildViews()
+    }
+    
+    private func buildViews(){
+        createViews()
+        styleViews()
+        defineLayout()
+        
+    }
+    
+    
+    func createViews() {
+        categories = [[MovieModel]]()
+        categories.append(details.popularMovies)
+        categories.append(details.freeToWatchMovies)
+        categories.append(details.trendingMovies)
+        
+        categorieTitles = ["What's popular", "Free to Watch", "Trending"]
+
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout() )
+        view.addSubview(collectionView)
+    }
+    
+    func styleViews() {
+        view.backgroundColor = .white
+        collectionView.register(MovieCategoryCell.self, forCellWithReuseIdentifier: MovieCategoryCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
+    }
+    
+    func defineLayout() {
+        collectionView.autoPinEdge(toSuperviewSafeArea: .top, withInset: 25)
+        collectionView.autoPinEdge(toSuperviewSafeArea: .leading, withInset: 16)
+        collectionView.autoPinEdge(toSuperviewSafeArea: .bottom, withInset: 45)
+        collectionView.autoPinEdge(toSuperviewSafeArea: .trailing)
+    }
+
+}
+
+extension MovieCategoriesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        3
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+     guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieCategoryCell.identifier, for: indexPath) as? MovieCategoryCell else { return UICollectionViewCell() }
+     
+     let categoryURL = categories[indexPath.section].map { URL(string: $0.imageUrl) }
+     let title = categorieTitles[indexPath.section]
+     cell.configure(with: categoryURL, categoryTitle: title)
+         
+     return cell
+   }
+}
+
+extension MovieCategoriesViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 374, height: 223)
+    }
+}
