2cc84d4f7c28670958e2189b25feae61b1b33293
anaskukan2 <ana.skukan7@gmail.com>
2023-05-04 13:22:22 +0200

    homework-2

---
+++ MovieApp/MovieListViewController.swift
@@ -0,0 +1,80 @@
+import Foundation
+import MovieAppData
+import UIKit
+
+class MovieListViewController : UIViewController {
+    
+    private var collectionView: UICollectionView!
+    let details = MovieUseCase()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buildViews()
+    }
+
+    private func buildViews(){
+        createViews()
+        styleViews()
+        defineLayout()
+    }
+    
+    private func createViews(){
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+    }
+    
+    private func styleViews(){
+        view.backgroundColor = .systemBackground
+        collectionView.backgroundColor = .systemBackground
+        view.backgroundColor = .white
+        collectionView.register(MovieListCell.self, forCellWithReuseIdentifier: MovieListCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+                
+    }
+    
+    private func defineLayout(){
+        collectionView.autoPinEdge(toSuperviewSafeArea: .top, withInset: 68)
+        collectionView.autoPinEdge(toSuperviewSafeArea: .leading, withInset: 16)
+        collectionView.autoPinEdge(toSuperviewSafeArea: .trailing, withInset: 16)
+        collectionView.autoPinEdge(toSuperviewSafeArea: .bottom, withInset: 58)
+    }
+        
+}
+
+extension MovieListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        details.allMovies.count
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+  
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieListCell.identifier, for: indexPath) as? MovieListCell else {
+          return UICollectionViewCell()
+        }
+        
+        let index = indexPath.row
+        let url = URL(string: details.allMovies[index].imageUrl)
+        let name = details.allMovies[index].name
+        let summary = details.allMovies[index].summary
+        cell.configureCell(url: url, name: name, summary: summary)
+        
+        return cell
+      
+    }
+
+}
+
+
+extension MovieListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 358, height: 142)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat{
+            12
+    }
+}
